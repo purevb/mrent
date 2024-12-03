8944d9477f47229a987f8b53172e76a352e8f721
Purevbaatar <purevbaatar@unitedalliance.mn>
2024-12-03 18:41:25 +0800

    firabse

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:mrent/firebase_options.dart';
 import 'package:mrent/pages/beginning_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 

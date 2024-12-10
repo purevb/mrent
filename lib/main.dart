ec1a5eaff5eff410070bb1e35f6dc8c7af329ed6
purevb <purevbaatar221@gmail.com>
2024-12-10 13:44:23 +0800

    bsda ce

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,14 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mrent/firebase_options.dart';
 import 'package:mrent/pages/beginning_page.dart';
 
-<<<<<<< HEAD
 void main() {
   debugPrintKeyboardEvents = false;
-=======
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // Future<void> main() async {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
 
->>>>>>> 8944d9477f47229a987f8b53172e76a352e8f721
   runApp(const MyApp());
 }
 
